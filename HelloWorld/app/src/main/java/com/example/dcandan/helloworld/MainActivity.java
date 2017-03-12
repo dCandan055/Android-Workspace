20482b822d3926820ef901769403ea31a06f6bd0
dCandan055 <dogan.candan@outlook.com>
2017-03-13 00:10:12 +0300

    Hello World

---
+++ HelloWorld/app/src/main/java/com/example/dcandan/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dcandan.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show();
+    }
+}
